b7c94c9ef40ebd8482139d065ca22ca734cbfc52
Administrator <416326391@qq.com>
2019-05-21 11:17:34 +0800

    alter

--- day01/grad.py
+++ day01/grad.py
@@ -4,19 +4,26 @@
 3.学习率
 """
 
+
 def get_grad(theta, x, y):
     """
     求梯度
     """
-    grad = 2 * (theta * x - y)
+    grad = 2*(theta * x - y)*x
     return -grad
 
 
 def get_cost(theta, x, y):
-    return (theta * x - y) * 2
+    """
+    学习率
+    """
+    return (theta*x - y)**2
 
 
 def gradient_descending(theta, x, y, learning_rate):
+    """
+   梯度下降
+    """
     theta = theta + get_grad(theta, x, y) * learning_rate
     return theta
 
@@ -25,7 +32,7 @@ y = 20
 x = 1.1
 theta = 0
 learning_rate = 0.1
-cost = get_grad(theta, x, y)
+cost = get_cost(theta, x, y)
 print(cost)
 theta = gradient_descending(theta, x, y, learning_rate)
 cost = get_cost(theta, x, y)
