6e491da992e36f6c74a2674429af3e721d5c5f4d
Administrator <416326391@qq.com>
2019-05-24 17:29:07 +0800

    alter

--- day01/aqi.py
+++ day01/aqi.py
@@ -9,28 +9,31 @@ import numpy as np
 import read_data
 from show import show_cost
 
-
 def get_grad(theta, x, y):
     """
     x:矩阵
     y:矩阵
     theta:矩阵
     """
-    grad = np.dot(np.transpose(x), (np.dot(x, theta) - y))
+    grad = np.dot(np.transpose(x), (np.dot(x, theta)-y))
     return grad
 
-
-def gradient_descending(theta, x, y, v_x, v_y, learning_rate):
+def gradient_descending(theta, x, y,v_x, v_y, learning_rate):
     """
     通过梯度下降算法，对线性回归模型进行训练
     """
-    train_costs = []  # 记录训练过程中产生的cost
-    validation_costs = []  # 记录验证集上产生的cost
+    train_costs = []     # 记录训练过程中产生的cost
+    validation_costs=[]    # 记录验证集上产生的cost
     for _ in range(200):
-        theta = theta - get_grad(theta, x, y) * learning_rate
+        theta = theta - get_grad(theta,x,y)*learning_rate
         train_costs.append(get_cost(theta, x, y))
         validation_costs.append(get_cost(theta, v_x, v_y))
     show_cost(train_costs, validation_costs)
+    # TODO: 将theta的值保存起来
+    with open('model.txt', 'w') as f:
+        for i in theta:
+            for j in i:
+                f.write(str(j)+"\n")
     return theta
 
 
@@ -38,7 +41,7 @@ def test_model(theta, test_x, test_y):
     """
     使用R方误差来测试模型的优劣
     """
-    r = 1 - get_cost(theta, test_x, test_y) / np.var(test_y)
+    r = 1 - get_cost(theta, test_x, test_y)/np.var(test_y)
     print(r)
 
 
@@ -51,25 +54,11 @@ def get_cost(theta, x, y):
     return np.mean((np.dot(x, theta) - y) ** 2)
 
 
-def get_aqi_value(input_data):
-    """
-    根据用户提供的输入数据，完成aqi值的预测
-    """
-    x = np.array(input_data)
-    x = read_data.standard_data(x)
-    # 从文件中读取theta
-    with open('model.txt', 'r') as f:
-        theta = np.array([float(line) for line in f.readlines()]).reshape(6, 1)
-    return np.dot(x, theta)
-
-
+'''
 train_data, validation_data, test_data = read_data.read_aqi()
 
-theta = np.zeros((6, 1))
-learning_rate = 0.00000011
-theta = gradient_descending(theta, train_data[0], train_data[1], validation_data[0], validation_data[1], learning_rate)
-# test_model(theta, test_data[0], test_data[1])
-aqi_value = get_aqi_value([33, 56, 7, 27, 0.82, 101])
-print(aqi_value)
-
-
+theta = np.zeros((6 ,1))
+learning_rate = 0.00001
+theta = gradient_descending(theta, train_data[0], train_data[1] , validation_data[0], validation_data[1], learning_rate)
+#test_model(theta, test_data[0], test_data[1])
+'''
--- day01/read_data.py
+++ day01/read_data.py
@@ -2,28 +2,24 @@
 """
 读取数据
 """
-import numpy as np
-import pandas as pd
 
+import pandas as pd
+import numpy as np
 
 def read_aqi():
-    aqi_data = pd.read_csv("aqi2.csv")  # 数据的读取
+    aqi_data = pd.read_csv("aqi2.csv")
     cols = ['PM2.5', 'PM10', 'CO', 'No2', 'So2', 'O3']
-    label = aqi_data["AQI"].values.reshape(-1, 1)  # reshape(-1, 1) 将数据转成列的形式
-    x = aqi_data[cols].values  # 获取cols的值
+    label = aqi_data["AQI"].values.reshape(-1, 1)
+    x = aqi_data[cols]
     x = x.apply(lambda data: np.log(data), axis=0).values
-    # x = (x - np.min(x) / (np.max(x) - np.min(x)))  # 固定公式，将数据标准化
     # 将数据拆分成3个部分，训练集占60%，验证集占30%，测试集占10%
     rows = len(x)
     train_rows = int(rows * 0.6)
-    validation_rows = int(rows * 0.3)
-    train_x, train_y = x[:train_rows], label[
-                                       :train_rows]  # y = [0:5]  取0到4, 5 取不到    y = [  : 6]  取0到5    y = [4: ]  取4到最后，最后一个可以取到
-    validation_x, validation_y = x[train_rows:train_rows + validation_rows], label[
-                                                                             train_rows:train_rows + validation_rows]
-    test_x, test_y = x[train_rows + validation_rows:], label[train_rows + validation_rows:]
+    validation_rows = int(rows*0.3)
+    train_x, train_y = x[:train_rows], label[:train_rows]
+    validation_x, validation_y = x[train_rows:train_rows+validation_rows], label[train_rows:train_rows+validation_rows]
+    test_x, test_y = x[train_rows+validation_rows:], label[train_rows+validation_rows:]
     return (train_x, train_y), (validation_x, validation_y), (test_x, test_y)
-
 
 def standard_data(input):
     """
