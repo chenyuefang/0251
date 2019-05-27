445e5f8ea0fddebac4aaf158813ae887f1c0d0e7
Administrator <416326391@qq.com>
2019-05-27 17:46:21 +0800

    alter

--- day01/02/cancer.py
+++ day01/02/cancer.py
@@ -1,6 +1,6 @@
 import numpy as np
-import data_reader
-
+from 逻辑回归 import data_reader
+from 逻辑回归 import show
 
 def sigmoid(x):
     return 1.0 / (1 + np.exp(-x))
@@ -8,22 +8,34 @@ def sigmoid(x):
 
 def get_cost(theta, x, y):
     h = sigmoid(np.dot(x, theta))
-    cost = np.sum(y * np.log(h) + (1 - y) * np.log(1 - h))
+    cost =np.sum( y * np.log(h) + (1 - y) * np.log(1 - h))
+    return -cost
 
 
-def get_grad(theta, x, y):
+def get_grade(theta, x, y):
     h = sigmoid(np.dot(x, theta))
     return np.dot(np.transpose(x), h - y)
 
 
-def gradient_descending(theta, x, y, learning_rate):
-    for _ in range(20):
-        theta = theta - learning_rate * get_grad(theta, x, y)
-        cost = get_cost(theta, x, y)
-        print (cost)
+def graident_descending(theta, x, y, learning_rate, batch_size, ecphocs = 20):
+    global cost
+    costs = []
+    batchs = len(x)//batch_size
+    if len(x)%batch_size != 0:
+        batchs += 1
+    for _ in range(ecphocs):
+        for batch in range(batchs):
+            start = batch*batch_size%len(x)
+            end = min(start+batch_size, len(x))
+            t_x = x[start:end]
+            t_y = y[start:end]
+            theta = theta - learning_rate * get_grade(theta, t_x, t_y)
+            cost = get_cost(theta, x, y)
+        costs.append(cost)
+    show.show_cost(costs)
 
 
 x, y = data_reader.read_data()
-theta = np.zeros(x.shape[1], 1)
-learning_rate = 0.001
-gradient_descending(theta, x, y, learning_rate)
+theta = np.zeros((x.shape[1], 1))
+learning_rate = 0.0001
+graident_descending(theta, x, y, learning_rate,batch_size=10)
---
+++ day01/02/show.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def show_cost(cost):
+    sns.set_style("whitegrid")
+    plt.plot(cost)
+    plt.show()
