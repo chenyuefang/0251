044cd28dd8228652396f223c821b1d8d575b906b
Administrator <416326391@qq.com>
2019-06-04 16:32:06 +0800

    alter

--- 06/bank_marketing1.py
+++ 06/bank_marketing1.py
@@ -1,30 +1,39 @@
-import tensorflow as tf
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+import tensorflow as tf
 
 
 def inference(input):
-    weights = tf.get_variable(name="weights",
-                              shape=[784, 10],
+    weights = tf.get_variable(name="weights1",
+                              shape=[784, 128],
                               initializer=tf.initializers.random_normal)
-    biases = tf.get_variable(name="biases",
-                             shape=[10],
+    biases = tf.get_variable(name="biases1",
+                             shape=[128],
                              initializer=tf.initializers.constant)
     x = tf.matmul(input, weights) + biases
-    return tf.nn.sigmoid(x)
-    weights2
+    x = tf.nn.sigmoid(x)
+    weights2 = tf.get_variable(name="weights2",
+                               shape=[128, 10],
+                               initializer=tf.initializers.random_normal)
+    biases2 = tf.get_variable(name="biases2",
+                              shape=[10],
+                              initializer=tf.initializers.constant)
+    x2 = tf.matmul(x, weights2) + biases2
+    x2 = tf.nn.sigmoid(x2)
+    return x2
 
 
-def train(input_x, input_y, ephocs=10, batch_size=100):
+def train(input_x, input_y, ephocs=10, batch_size=1000):
     x = tf.placeholder("float", shape=[None, input_x.shape[1]], name="x-input")
     y = tf.placeholder("float", shape=[None, input_y.shape[1]], name="y-input")
     output = tf.nn.softmax(inference(x))
     # 定义损失函数
-    cost = -tf.reduce_sum(y * tf.log(output) + (1 - y) * tf.log(1 - output))
-    entropy_cost = tf.train.GradientDescentOptimizer(0.0003).minimize(cost)
+    cost = -tf.reduce_mean(y * tf.log(output))  # 逻辑回归的损失函数
+    entropy_cost = tf.train.AdamOptimizer(0.003).minimize(cost)
+    #  定义准确率的验证
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(output, axis=1), tf.argmax(y, axis=1)), "float"))
     batches = input_x.shape[0] // batch_size
     if input_x.shape[0] % batch_size != 0:
         batches += 1
-
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
         for _ in range(ephocs):
@@ -32,8 +41,11 @@ def train(input_x, input_y, ephocs=10, batch_size=100):
                 start = batch * batch_size % input_x.shape[0]
                 end = min(start + batch_size, input_x.shape[0])
                 sess.run([entropy_cost], feed_dict={x: input_x[start:end], y: input_y[start:end]})
-            print(sess.run([cost], feed_dict={x: input_x, y: input_y}))
+            c = sess.run([cost, accuracy], feed_dict={x: input_x, y: input_y})
+            print(c)
 
 
 mnist = read_data_sets("data\\", one_hot=True)
 train(mnist.train.images, mnist.train.labels)
+# print(mnist.train.images[0])
+# print(mnist.train.labels[0])
